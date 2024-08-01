0e9529cd6b295e9083819066e4f44e8177d90b4c
Daniel Henderson <djhindu17@gmail.com>
2024-08-01 13:58:06 -0500

    Assessment complete/comment cleanup to show the steps via the assessment parameters.

--- src/scripts/PotteryList.js
+++ src/scripts/PotteryList.js
@@ -1,9 +1,12 @@
 import { usePottery } from "./PotteryCatalog.js";
 
+
+// 2. Define and export a PotteryList function.
 export const PotteryList = () => {
+    //3. The PotteryList function must get the items to be sold from the PotteryCatalog.js module.
     const potteryToSell = usePottery();
     let potteryHTML = "";
-
+    // 4. The PotteryList function must convert each object in the array to an HTML representation string. Use the following template to generate the representations.
     for (const potteryItem of potteryToSell) {
         potteryHTML += `
             <section class="pottery" id="pottery--${potteryItem.id}">
@@ -15,6 +18,6 @@ export const PotteryList = () => {
             </section>
         `
     }
-
+    // 5. The PotteryList function must then return a single string that contains ALL of the pottery HTML representation.
     return potteryHTML;
 };
